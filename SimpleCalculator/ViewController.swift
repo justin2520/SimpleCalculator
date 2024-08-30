3b7f9c543ecaa43de7e17a814c11302d78e2746e
Justin Weber <justinweber805@outlook.com>
2024-08-30 07:33:27 -0500

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by JUSTIN WEBER on 8/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
