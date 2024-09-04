506f91e714a47f5f2d39bf65a8a74bd6e79063bd
Justin Weber <justinweber805@outlook.com>
2024-09-04 18:38:54 -0500

    First commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numOneTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var numTwoTextFieldOutlet: UITextField!
+    
+    @IBOutlet weak var operatorLabelOutlet: UILabel!
+    
+    @IBOutlet weak var resultLabelOutlet: UILabel!
+    
+    @IBOutlet weak var errorLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func addButton(_ sender: UIButton) {
+        if Int(numOneTextFieldOutlet.text!) != nil && Int(numTwoTextFieldOutlet.text!) != nil{
+            resultLabelOutlet.text = String(Int(numOneTextFieldOutlet.text!)! + Int(numTwoTextFieldOutlet.text!)!)
+            
+            operatorLabelOutlet.text = "+"
+            errorLabelOutlet.text = ""
+        }
+        else{
+            errorLabelOutlet.text = "Error!"
+            
+            numOneTextFieldOutlet.text = ""
+            numTwoTextFieldOutlet.text = ""
+            resultLabelOutlet.text = ""
+            operatorLabelOutlet.text = ""
+        }
+    }
+    
+    @IBAction func subtractButton(_ sender: UIButton) {
+        if Int(numOneTextFieldOutlet.text!) != nil && Int(numTwoTextFieldOutlet.text!) != nil{
+            resultLabelOutlet.text = String(Int(numOneTextFieldOutlet.text!)! - Int(numTwoTextFieldOutlet.text!)!)
+            
+            operatorLabelOutlet.text = "-"
+            errorLabelOutlet.text = ""
+        }
+        else{
+            errorLabelOutlet.text = "Error!"
+            
+            numOneTextFieldOutlet.text = ""
+            numTwoTextFieldOutlet.text = ""
+            resultLabelOutlet.text = ""
+            operatorLabelOutlet.text = ""
+        }
+    }
+    
+    @IBAction func multiplyButton(_ sender: UIButton) {
+        if Int(numOneTextFieldOutlet.text!) != nil && Int(numTwoTextFieldOutlet.text!) != nil{
+            resultLabelOutlet.text = String(Int(numOneTextFieldOutlet.text!)! * Int(numTwoTextFieldOutlet.text!)!)
+            
+            operatorLabelOutlet.text = "x"
+            errorLabelOutlet.text = ""
+        }
+        else{
+            errorLabelOutlet.text = "Error!"
+            
+            numOneTextFieldOutlet.text = ""
+            numTwoTextFieldOutlet.text = ""
+            resultLabelOutlet.text = ""
+            operatorLabelOutlet.text = ""
+        }
+    }
+    
+    @IBAction func divideButton(_ sender: UIButton) {
+        if Int(numOneTextFieldOutlet.text!) != nil && Int(numTwoTextFieldOutlet.text!) != nil{
+            resultLabelOutlet.text = String(Double(numOneTextFieldOutlet.text!)! / Double(numTwoTextFieldOutlet.text!)!)
+            
+            operatorLabelOutlet.text = "÷"
+            errorLabelOutlet.text = ""
+        }
+        else{
+            errorLabelOutlet.text = "Error!"
+            
+            numOneTextFieldOutlet.text = ""
+            numTwoTextFieldOutlet.text = ""
+            resultLabelOutlet.text = ""
+            operatorLabelOutlet.text = ""
+        }
+    }
 }
 
